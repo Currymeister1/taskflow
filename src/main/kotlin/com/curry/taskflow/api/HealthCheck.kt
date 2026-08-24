56928e606beceab7ee6d559e5b5ce520409de7c0
Currymeister1 <73665964+Currymeister1@users.noreply.github.com>
2026-08-24 21:07:26 +0200

    Health check

---
+++ src/main/kotlin/com/curry/taskflow/api/HealthCheck.kt
@@ -0,0 +1,15 @@
+package com.curry.taskflow.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@SpringBootApplication
+@RequestMapping("/api")
+class HealthCheck {
+
+    @GetMapping("/health")
+    fun health() = mapOf("status" to "UP")
+}
